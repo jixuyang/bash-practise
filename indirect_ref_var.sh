d28290fb4670d743306c119dee3413e669c0b50c
jixu_yang <jixu_yang@aliyun.com>
2017-09-07 22:28:41 +0800

    Make script work

--- indirect_ref_var.sh
+++ indirect_ref_var.sh
@@ -4,15 +4,16 @@
 # verify how to indirectly refer variable
 #
 
-info1="It is var \"info1\""
-info2="It is var \"info2\""
-info3="It is var \"info3\""
-info4="It is var \"info4\""
+info1="It is var \"info1\"."
+info2="It is var \"info2\"."
+info3="It is var \"info3\"."
+info4="It is var \"info4\"."
 
 i=1
 while [[ $i -lt 5 ]]; do
   var=info$i
-  echo ${!var}
+  echo "$var = ${!var}"
+  i=$((i+1))
 done
 
 exit
